e7b6908d300c342e0a46b406a1fe3536e6290b0c
ishl250 <ishimwejeanclaude088@gmail.com>
2025-10-30 11:36:00 +0300

    Add files via upload

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,63 @@
+import 'package:agriplant/pages/orders_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 15),
+            child: CircleAvatar(
+              radius: 62,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              child: const CircleAvatar(
+                radius: 60,
+                foregroundImage: NetworkImage(
+                    'assets/hantapro.jpg'),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "ish licky",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Center(
+            child: Text(
+              "ish@gmail.com",
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ),
+          const SizedBox(height: 25),
+          ListTile(
+            title: const Text("My orders"),
+            leading: const Icon(IconlyLight.bag),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OrdersPage(),
+                  ));
+            },
+          ),
+          ListTile(
+            title: const Text("About us"),
+            leading: const Icon(IconlyLight.infoSquare),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text("Logout"),
+            leading: const Icon(IconlyLight.logout),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
